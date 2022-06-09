acfe386406035d6db05a2153db309087e1841e8c
Samuel Adamson <saad2392@colorado.edu>
2022-06-08 21:26:43 -0600

    Added ye old cell class - researching MVC, Razor Components

---
+++ minesweeper/minesweeper/Models/Cell.cs
@@ -0,0 +1,35 @@
+﻿/*  Cell Class
+ *      - Cells used in gameboard
+ *      - Model class - hidden from frontend
+ *  Samuel Adamson 
+ */
+
+namespace minesweeper.Models
+{
+    public class Cell
+    {
+        // Properties of cell
+        bool mine; // Is this cell a mine
+        bool revealed; // Is the cell revealed
+        int adjacent; // Number of adjacent mines
+        int row, col; // Coordinates of cell in grid
+
+        /// <summary>
+        /// Cell constructor
+        /// </summary>
+        /// <param name="row"> Cell Row </param>
+        /// <param name="col"> Cell Column </param>
+        public Cell(int row, int col)
+        {
+            // Update row, col
+            this.row = row;
+            this.col = col;
+
+            // Initialize properties
+            mine = false;
+            revealed = false;
+            adjacent = 0;
+        }
+
+    }
+}
