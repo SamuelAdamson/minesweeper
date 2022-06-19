f7b727e3e08134902c14a175e52ef7f301282874
Samuel Adamson <saad2392@colorado.edu>
2022-06-18 23:27:48 -0600

    Created Mode UI selector

---
+++ minesweeper/minesweeper/Models/CustomEventArgs/ModeEventArgs.cs
@@ -0,0 +1,22 @@
+﻿/*  Mode Event Args Class
+ *      - Custom event argument for mode selection
+ *  Samuel Adamson 
+ */
+
+namespace minesweeper.Models.CustomEventArgs
+{
+	public class ModeEventArgs : EventArgs
+	{
+		// New mode that was selected
+		string newMode;
+
+		public ModeEventArgs(string newMode)
+		{
+			// Set Values
+			this.newMode = newMode;
+		}
+
+		// Access Control
+		public string NewMode { get => newMode; set => newMode = value; }
+	}
+}
