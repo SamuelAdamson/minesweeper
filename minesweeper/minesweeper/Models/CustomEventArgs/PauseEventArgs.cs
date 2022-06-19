e41c4fcaa9bdcb36880b4e0f57accc035ac32480
Samuel Adamson <saad2392@colorado.edu>
2022-06-19 14:36:07 -0600

    Added Timer and Pause Button

---
+++ minesweeper/minesweeper/Models/CustomEventArgs/PauseEventArgs.cs
@@ -0,0 +1,22 @@
+﻿/*  Pause Event Args Class
+ *      - Custom event argument for puasing
+ *  Samuel Adamson 
+ */
+
+namespace minesweeper.Models.CustomEventArgs
+{
+    public class PauseEventArgs : EventArgs
+    {
+        // Event message
+        string message;
+
+        public PauseEventArgs(string message)
+        {
+            // Set fields
+            this.message = message;
+        }
+
+        // Access control
+        public string Message { get => message; set => message = value; }
+    }
+}
