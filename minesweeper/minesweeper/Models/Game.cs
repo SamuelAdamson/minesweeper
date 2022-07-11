54c458b50597faa7f6d2e1c2d08a90e325017e77
Samuel Adamson <saad2392@colorado.edu>
2022-07-10 22:33:59 -0600

    Added some about text, some more logic
    Struggling with component rerendering -- getting several renders occurring on StateHasChanged calls

--- minesweeper/minesweeper/Models/Game.cs
+++ minesweeper/minesweeper/Models/Game.cs
@@ -26,7 +26,7 @@ namespace minesweeper.Models
         int numMines;
 
         // Coordinates of mines
-        List<Tuple<int,int>> mineCoords;
+        HashSet<Tuple<int,int>> mineCoords;
 
         // Random Generator
         Random random;
@@ -37,6 +37,7 @@ namespace minesweeper.Models
         /// <param name="mode"> Mode [easy, medium, hard] </param>
         public Game(string mode)
         {
+            System.Diagnostics.Debug.WriteLine("Constructor");
             // Initialize random generator
             random = new Random();
             firstSelect = true; // First selection is set to true
@@ -61,21 +62,21 @@ namespace minesweeper.Models
 
             if(modeStr == "easy") {
                 // 8x12 Grid
-                // 8 Mines
+                // 10 Mines
                 nRows = 8;
                 nCols = 12;
-                numMines = 8;
+                numMines = 10;
 			}
             else if(modeStr == "medium") {
                 // 12x16 Grid
-                // 20 Mines
+                // 40 Mines
                 nRows = 12;
                 nCols = 16;
                 numMines = 40;
             }
             else {
                 // 16x20 Grid
-                // 50 Mines
+                // 80 Mines
                 nRows = 16;
                 nCols = 20;
                 numMines = 80;
@@ -106,12 +107,15 @@ namespace minesweeper.Models
         public int NCols { get => nCols; }
         public string Mode { get => mode; }
 
+        
 
         /// <summary>
-        /// Get adjacent coordinates of cells that are uncovered
+        /// Get adjacent cells from a specific coordinate
+        /// If a search, mark each cell as visited
         /// </summary>
-        /// <param name="coord"> Coordinate </param>
-        /// <returns> Adjacent Coordinates </returns>
+        /// <param name="coord"> Coordinate to Find Adjacent Cells</param>
+        /// <param name="search"> Called in a search </param>
+        /// <returns></returns>
         private List<Tuple<int, int>> GetAdjacentCoords(Tuple<int, int> coord)
 		{
             // Adjacent coordinates
@@ -123,8 +127,8 @@ namespace minesweeper.Models
                 for(int j = coord.Item2 - 1; j <= coord.Item2 + 1; j++)
 				{
                     if((i >= 0 && j >= 0 && i < nRows && j < nCols) // Ensure within bounds
-                        && !(i == coord.Item1 && j == coord.Item2) // Ensure not coordinate itself
-                        && grid[i,j].Covered) // Ensure cell is not covered
+                        && !(i == coord.Item1 && j == coord.Item2) // Ensure not coordinate itself)                        
+                        && grid[i,j].Covered) // Ensure cell is covered
                     {
                         adjacents.Add(new Tuple<int, int>(i,j));
 			        }
@@ -159,8 +163,8 @@ namespace minesweeper.Models
         private void PlaceMines()
 		{
 			// Set random coordinates
-            mineCoords = new List<Tuple<int, int>>(); // Create list of coordinates
-            SetRandomCoordinates(numMines, mineCoords); // Set random coords, pass mineCoords by ref
+            mineCoords = new HashSet<Tuple<int, int>>(); // Create list of coordinates
+            SetRandomCoordinates(numMines, mineCoords, false); // Set random coords, pass mineCoords by ref
 
             // Iterate coordinates
             foreach(Tuple<int,int> coord in mineCoords)
@@ -188,29 +192,32 @@ namespace minesweeper.Models
 
             // Add new random coordinate
             int n = mineCoords.Count;
-            SetRandomCoordinates(n + 1, mineCoords);
-            PlaceMine(mineCoords[n - 1]);
+            PlaceMine(SetRandomCoordinates(n + 1, mineCoords, true));
 		}
 
         /// <summary>
         /// Set random coordinates
+        /// Produces a HashSet of n unique random coordinates
+        /// If replace is true, the last coordinate added will be returned from the function
         /// </summary>
         /// <param name="n"> Number of coordinates </param>
-        /// <param name="coords"> Current List (By Reference) </param>
-		private void SetRandomCoordinates(int n, List<Tuple<int, int>> coords)
+        /// <param name="coords"> Hash Set of Coordinates </param>
+        /// <param name="replace"> Replace if True last coordinate returned </param>
+        /// <returns></returns>
+		private Tuple<int,int>? SetRandomCoordinates(int n, HashSet<Tuple<int, int>> coords, bool replace)
 		{
-			while (coords.Count <= n)
-			{
-				// Create random coordinate
-				Tuple<int, int> coord = new Tuple<int, int>(random.Next(nRows), random.Next(nCols));
-				do
-				{
-					coord = new Tuple<int, int>(random.Next(nRows), random.Next(nCols));
-				} while (coords.Contains(coord)); // Run while non-unique coordinates
+            // If replace is true, this value will be returned
+            Tuple<int, int>? newCoord = null;
 
-                // Add coord
+			while (coords.Count < n)
+            {   // Using hashset, so only unique items will be used (O(n) as opposed to O(n^2))
+                Tuple<int, int> coord = new Tuple<int, int>(random.Next(nRows), random.Next(nCols));
                 coords.Add(coord);
-			}
+
+                if(replace) { newCoord = coord; }
+            }
+
+            return newCoord;
 		}
 
         /// <summary>
@@ -227,12 +234,26 @@ namespace minesweeper.Models
             } 
 
             // Check if none adjacent
+            /* If a cell is uncovered that is not adjacent to any mines,
+             *  then all cells adjacent to this cell are also uncovered.
+             *  This same property applies to cells that are uncovered 
+             *  in the process.
+             */
             if(((Cell)sender).Adjacent == 0)
             {
-
+                
             }
         }
 
 
+        private void BreadthFirstSearch(Tuple<int,int> coord)
+        {
+            // TODO
+        }
+
+        private void DepthFirstSearch(Tuple<int,int> coord)
+        {
+            // TODO
+        }
 	}
 }
